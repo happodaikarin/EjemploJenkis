a4bdff0946403e6f0c0e08fd7bd2e88ca58cfa92
Ramses <rambandan@gmail.com>
2024-11-05 07:47:41 -0500

    miprimerprojectojenkis2

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -11,4 +11,11 @@ public class AppTest {
         int result = app.add(5, 3);
         assertEquals(8, result); // Verifica que 5 + 3 = 8
     }
+
+    @Test
+    public void testDiv() {
+        App app = new App();
+        int result = app.div(10, 2);
+        assertEquals(5, result); // Verifica que 10 / 2 = 5
+    }
 }
